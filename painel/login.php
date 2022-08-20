2208a35f2f27c39bd9e6f43c178d8f042fea6199
João Vitor <jv2005.jvar@gmail.com>
2022-08-19 22:38:30 -0300

    método de logout

--- painel/login.php
+++ painel/login.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/font-awesome.css">
     <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL; ?>css/painel-estilo.css">
     <title>Login</title>
 </head>
@@ -33,7 +35,7 @@
                     die();
                 }else{
                     //fail
-                    echo "<div class='box-error'><p>Usuário ou senha inválidos</p></div>";
+                    echo "<div class='box-error'><p><i class='fa fa-times'></i>Usuário ou senha inválidos</p></div>";
                 }
             }
         ?>
@@ -42,8 +44,27 @@
             <input type="text" name="user" placeholder="Username..." required>
             <br>
             <input type="password" name="password" placeholder="Senha..." required>
+            <img name="show-password-login" src=../images/olho-aberto.png />
             <button type="submit" name="acao">Entrar!</button>
         </form>
     </div>
 </body>
+<script>
+    const img = document.querySelector('img');
+    const inputPasswd = document.querySelector('input[type=password]');
+
+    img.addEventListener('click', ()=>{ 
+    img.classList.toggle('show');
+    if(img.classList.contains('show')){
+        //mudar input para texto
+        img.src = "../images/olho-fechado.png";
+        inputPasswd.type = "text";
+    }else{
+        //mudar input para password
+        img.src = "../images/olho-aberto.png";
+        inputPasswd.type = "password";
+    }
+})
+
+</script>
 </html>
--- painel/main.php
+++ painel/main.php
@@ -1,1 +1,32 @@
-<h2>Bem vindo ao painel <?php echo $_SESSION['user'] ?></h2>
+<?php
+	if(isset($_GET['logout'])){
+		Painel::logout();
+	}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL; ?>css/painel-estilo.css">
+    <title>Painel de controle</title>
+</head>
+<body>
+	<aside>
+
+	</aside>
+	<header>
+		<div class="center">	
+			<div class="logout">
+				<a href="<?php echo INCLUDE_PATH_PAINEL; ?>?logout">Logout <i class="fa fa-window-close"></i></a>
+			</div> <!-- logout -->
+			<div class="clear"></div>
+		</div>
+	</header>
+	<div class="clear"></div>
+</body>
+</html>
